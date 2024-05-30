a62e47e749ca995cdf24ef751b13bcbb780e4633
alex4401 <rylatgl@gmail.com>
2024-05-30 22:25:03 +0200

    feat: show a warning above the editor form when editing global messages

--- src/Hooks/EditorHooks.php
+++ src/Hooks/EditorHooks.php
@@ -1,10 +1,14 @@
 <?php
 namespace MediaWiki\Extension\GlobalMessages\Hooks;
 
+use EditPage;
+use Html;
+use OutputPage;
 use Title;
 
 final class EditorHooks implements
-    \MediaWiki\Hook\EditFormPreloadTextHook
+    \MediaWiki\Hook\EditFormPreloadTextHook,
+    \MediaWiki\Hook\EditPage__showEditForm_initialHook
 {
     /**
      * @param string &$text Text to preload with
@@ -18,5 +22,19 @@ final class EditorHooks implements
                 $text = $msg->plain();
             }
         }
+    }
+
+	/**
+	 * Adds a warning above the edit field for global messages.
+	 *
+	 * @param EditPage $editor
+	 * @param OutputPage $out OutputPage instance to write to
+	 * @return bool|void True or no return value without altering $error to allow the
+	 *   edit to continue. Modifying $error and returning true will cause the contents
+	 *   of $error to be echoed at the top of the edit form as wikitext. Return false
+	 *   to halt editing; you'll need to handle error messages, etc. yourself.
+	 */
+	public function onEditPage__showEditForm_initial( $editor, $out ) {
+        $out->addHTML( Html::warningBox( wfMessage( 'globalmsg-onmsgedit' ) ) );
     }
 }
